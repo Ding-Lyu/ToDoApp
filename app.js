bc108f242acd9b342dce7150836ec385af2c1036
Ding-Lyu <lyudingguangnz@outlook.com>
2022-05-17 15:38:09 +1200

    All functions added

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ add.addEventListener("click", (e) => {
   let todoMonth = form.children[1].value;
   let todoDate = form.children[2].value;
 
+  if (todoText === "") {
+    alert("Please input valid event!");
+    return;
+  }
+
   //create a todo
   let todo = document.createElement("div");
   todo.classList.add("todo");
@@ -27,5 +32,118 @@ add.addEventListener("click", (e) => {
   todo.appendChild(text);
   todo.appendChild(time);
 
+  //create green check and red trash icon
+  let completeButton = document.createElement("button");
+  completeButton.classList.add("complete");
+  completeButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+  completeButton.addEventListener("click", (e) => {
+    let todoItem = e.target.parentElement;
+    todoItem.classList.toggle("done");
+  });
+
+  let trashbutton = document.createElement("button");
+  trashbutton.classList.add("trash");
+  trashbutton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+  trashbutton.addEventListener("click", (e) => {
+    let todoItem = e.target.parentElement;
+
+    todoItem.addEventListener("animationend", () => {
+      let text = todoItem.children[0].innerText;
+      let myListArray = JSON.parse(localStorage.getItem("list"));
+      myListArray.forEach((item, index) => {
+        if (item.todoText == text) {
+          myListArray.splice(index, 1);
+          localStorage.setItem("list", JSON.stringify(myListArray));
+        }
+      });
+
+      todoItem.remove();
+    });
+
+    todoItem.style.animation = "scaleDown 1s forwards";
+  });
+
+  todo.appendChild(completeButton);
+  todo.appendChild(trashbutton);
+
+  todo.style.animation = "scaleUp 1s forwards ";
+
+  //create an object
+  let myTodo = {
+    todoText: todoText,
+    todoMonth: todoMonth,
+    todoDate: todoDate,
+  };
+
+  // Store data into an Array on the local machine.
+  let myList = localStorage.getItem("list");
+  if (myList == null) {
+    localStorage.setItem("list", JSON.stringify([myTodo]));
+  } else {
+    let myListArray = JSON.parse(myList);
+    myListArray.push(myTodo);
+    localStorage.setItem("list", JSON.stringify(myListArray));
+  }
+
+  console.log(JSON.parse(localStorage.getItem("list")));
+
+  //Clear the text input
+  form.children[0].value = " ";
+
   section.appendChild(todo);
 });
+
+let myList = localStorage.getItem("list");
+if (myList !== null) {
+  let myListArray = JSON.parse(myList);
+  myListArray.forEach((item) => {
+    //create a todo
+    let todo = document.createElement("div");
+    todo.classList.add("todo");
+    let text = document.createElement("p");
+    text.classList.add("todo-text");
+    text.innerText = item.todoText;
+    let time = document.createElement("p");
+    time.classList.add("todo-time");
+    time.innerText = item.todoMonth + " / " + item.todoDate;
+    todo.appendChild(text);
+    todo.appendChild(time);
+
+    //create green check and red trash icon
+    let completeButton = document.createElement("button");
+    completeButton.classList.add("complete");
+    completeButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+
+    completeButton.addEventListener("click", (e) => {
+      let todoItem = e.target.parentElement;
+      todoItem.classList.toggle("done");
+    });
+
+    let trashbutton = document.createElement("button");
+    trashbutton.classList.add("trash");
+    trashbutton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    trashbutton.addEventListener("click", (e) => {
+      let todoItem = e.target.parentElement;
+
+      todoItem.addEventListener("animationend", () => {
+        //remove from local storage
+        let text = todoItem.children[0].innerText;
+        let myListArray = JSON.parse(localStorage.getItem("list"));
+        myListArray.forEach((item, index) => {
+          if (item.todoText == text) {
+            myListArray.splice(index, 1);
+            localStorage.setItem("list", JSON.stringify(myListArray));
+          }
+        });
+
+        todoItem.remove();
+      });
+
+      todoItem.style.animation = "scaleDown 1s forwards";
+    });
+    todo.appendChild(completeButton);
+    todo.appendChild(trashbutton);
+
+    section.appendChild(todo);
+  });
+}
